3bac1ff8b1ce9a136b0aa1d13c4e4a56f84c854b
Harsh shinde <shindeharsh.dev@gmail.com>
2026-08-09 10:04:28 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,60 +1,67 @@
 class Solution {
+    public int search(int[] nums, int target) {
 
-    public static int search(int[] nums, int target) {
+            int l =0;
+            int h= nums.length-1;
+            int pivot = pivotIndex(nums);
+
+            if(pivot == -1){
+
+                return BinarySearch(nums,target,l,h);
+            }
+
+            if( nums[pivot] == target){
+                return pivot;
+            }
+            if( target >= nums[l]){
+
+               return  BinarySearch(nums,target ,l,pivot-1);
+            }
+          
+         return   BinarySearch(nums,target ,pivot+1,h);
+         
+            
        
-        int pivot = findpivot(nums);
-
- if (pivot == -1) {
-            return binarySearch(nums, target, 0, nums.length-1); // this was my main mistake i wrote this condition after checking =trgt which casued -1== 0 . array outof box exception
-        }
-        if (nums[pivot] == target) {
-            return pivot; // here i tried to return the element; first
-        }
-       
-
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-
-        }
-        return binarySearch(nums, target, pivot + 1, nums.length-1);
-
     }
-
-    public static int binarySearch(int[] nums, int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
+    public int BinarySearch(int[] nums, int target , int l ,int h) {
+   
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (target == nums[mid]) {
                 return mid;
-            } else if (target < nums[mid]) {
-                end = mid - 1;
+            }
+           else if (nums[mid] > target) {
+                h = mid - 1;
             } else {
-                start = mid + 1;
-            }
-        }
-
-        return -1;
-    }
-
-    public static int findpivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        int mid = -1;
-        while (start <= end) {
-            mid = start + (end - start )/ 2;
-
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            } else if (mid > start && arr[mid - 1] > arr[mid]) {
-                return mid - 1;
-
-            } else if (arr[start] >= arr[mid]) { // here i made mistake..
-                end = mid - 1;
-            }
-            if (arr[start] < arr[mid]) {
-                start = mid + 1;
+                l = mid + 1;
             }
         }
         return -1;
     }
+
+    public int pivotIndex (int [] nums){
+        
+        int l=0;
+        int h= nums.length-1;
+          int pivot =-1;
+        while( l <= h){
+      
+        int mid = l + ( h - l) /2;
+
+        if( mid < h && nums[mid] > nums[mid+1]){
+          return mid;
+        }
+        else if(mid > l && nums[mid-1] > nums[mid]){
+            return mid-1;
+        }
+        else if(nums[l] <= nums[mid]){
+            l = mid+1;
+        }
+     if(nums[h] >= nums[mid]){
+            h = mid-1;
+        }
+      
+    }
+    return -1;
+}
 }
