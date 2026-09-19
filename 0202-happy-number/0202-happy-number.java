dbee336c064dffc3f86430406adf85792a74fbb1
Harsh shinde <shindeharsh.dev@gmail.com>
2026-09-19 15:57:37 +0530

    Time: 1 ms (77.7%), Space: 42.6 MB (21.44%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,25 +1,23 @@
 class Solution {
-    
-    public int sumOf(int n){
-        int sum=0;
-        int digit=0;
-        while( n !=0){
-            digit = n %10;
-            sum += digit * digit;
-            n = n/10;
+    public boolean isHappy(int n) {
+
+    int fast =n;
+    int slow =n;
+
+    do{
+ fast = isSum(isSum(fast));
+ slow = isSum(slow);
+    }
+    while(fast != slow);
+    return slow==1;
+    }
+    public int isSum(int n){
+        int sum =0;
+        while( n!=0){
+        int digit = n %10;
+        sum += digit * digit;
+        n = n /10;
         }
         return sum;
-    }
-    
-    public boolean isHappy(int n) {
-        int Fast = n;
-        int Slow = n;
-
-do{
-    Slow = sumOf(Slow);
-    Fast = sumOf(sumOf(Fast));
-}
-    while( Fast != Slow);
-    return Slow==1;
     }
 }
