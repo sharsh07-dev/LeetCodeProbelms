a742bbcc59438f7819699e2e2fb2d2d97b23175b
Harsh shinde <shindeharsh.dev@gmail.com>
2026-09-22 18:39:24 +0530

    Time: 3 ms (43.18%), Space: 46.6 MB (29.4%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,47 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+       Stack <Integer> s1 = new Stack <>();
+       Stack <Integer> s2 = new Stack <>();
+
+        while(l1 != null){
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+       ListNode head = null;
+        int carry =0;
+        while( !s1.isEmpty() || !s2.isEmpty() || carry !=0){
+
+               int sum = carry;
+
+                if(!s1.isEmpty()){
+                    sum+= s1.pop();
+                }
+                if(!s2.isEmpty()){
+                    sum+= s2.pop();
+                }
+
+            
+
+            ListNode node = new ListNode(sum %10);
+
+            node.next = head;
+            head = node;
+
+carry = sum /10;
+        }
         
-    ListNode rev1 = reverse(l1);
-     ListNode  rev2 = reverse(l2);
-        ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-        
-        int carry= 0;
-        while( rev1 != null || rev2 != null || carry !=0){
-
-            int sum = carry;
-            if(rev1 != null){
-                sum+= rev1.val;
-                rev1 = rev1.next;
-            }
-             if(rev2 != null){
-                sum+= rev2.val;
-                rev2 = rev2.next;
-            }
-                carry = sum /10;
-                temp.next = new ListNode(sum %10);
-                temp = temp.next;
-        }
-     ListNode real = reverse(dummy.next);
-     return real;
-    
-    }
-     public ListNode reverse(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
-        }
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while( curr != null){
-
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-
-        }
-        return prev;
-    }
+    return head; }
+   
 }
